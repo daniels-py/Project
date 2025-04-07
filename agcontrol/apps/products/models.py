f4fb566ea6296a46f56b78fcda7ea2c406d7ebbc
daniels-py <danielramirezs5518@gmail.com>
2025-04-07 18:23:44 -0500

    feat(products) nuevos campos para producto

--- agcontrol/apps/products/models.py
+++ agcontrol/apps/products/models.py
@@ -7,6 +7,9 @@ from django.db import models
 class Producto(models.Model):
     nombre = models.CharField(max_length=200, verbose_name='Nombre del producto')
     descripcion = models.CharField(max_length=200, verbose_name='Descripción del producto')
+    precio = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    codigo = models.CharField(max_length=100, unique=True, null=True, blank=True)
 
     def __str__(self):
         return self.nombre
+
