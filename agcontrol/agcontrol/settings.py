f489f29b60451b2210374f03ccdef95e150e47b9
daniels-py <danielramirezs5518@gmail.com>
2025-04-07 16:46:07 -0500

    feat(core) nuevos modelos marca, categorias, presentacion

--- agcontrol/agcontrol/settings.py
+++ agcontrol/agcontrol/settings.py
@@ -53,6 +53,7 @@ THIRD_PARTY_APPS = [
 LOCAL_APPS = [
     'apps.users',
     'apps.products',
+    'apps.core',
 ]
 
 # esto es para unir todas las apps en una sola lista
--- agcontrol/apps/core/models.py
+++ agcontrol/apps/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Marca(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+class Presentacion(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.nombre
