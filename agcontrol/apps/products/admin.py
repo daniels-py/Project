fc5c10ebd0766304c2772716951b18966be049b8
daniels-py <danielramirezs5518@gmail.com>
2025-04-17 21:38:37 -0500

    feat(core) update en panel admin

--- agcontrol/apps/products/admin.py
+++ agcontrol/apps/products/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Producto  # Importamos el modelo Producto
+from .models import Producto, CaracteristicaColor# Importamos el modelo Producto
 
 # Register your models here.
 
 admin.site.register(Producto)  # Registramos el modelo en el panel de administración
+admin.site.register(CaracteristicaColor)
