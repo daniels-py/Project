f08c083b902ede84efc23992ec2fa88d84df9950
daniels-py <danielramirezs5518@gmail.com>
2025-07-10 13:14:25 -0500

    feat: permiso personalizado integrado

--- agcontrol/apps/sales/views.py
+++ agcontrol/apps/sales/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics, permissions
 from .models import Venta
 from .serializers import VentaSerializer
+from permissions.permissions import IsAdminUser  # mi permiso personalizado
+
 
 
 # Crear una venta con sus detalles
 class CrearVentaView(generics.CreateAPIView):
     queryset = Venta.objects.all()
     serializer_class = VentaSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAdminUser]
+
 
     def perform_create(self, serializer):
         # Asigna automáticamente el usuario que hace la venta
@@ -17,7 +20,9 @@ class CrearVentaView(generics.CreateAPIView):
 # Listar las ventas realizadas por el usuario autenticado
 class MisVentasView(generics.ListAPIView):
     serializer_class = VentaSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAdminUser]
+
+
 
     def get_queryset(self):
         return Venta.objects.filter(usuario=self.request.user).order_by('-fecha')
